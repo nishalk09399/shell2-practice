8d7a67829798f2d58ee2a8e3c1bf576dc1d4ac28
nishal <nishalk9399@gmail.com>
2023-08-18 19:55:00 +0530

    root

---
+++ 08-install-git.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#goal is to install the mysql
+
+USERID=$(id -u)
+if [ $USERID -ne 0 ]
+then
+   echo "Stop immediately from the installation"
+else
+  echo "Proceed for the mysql server installation "
+fi
+
+
+yum install git -y
