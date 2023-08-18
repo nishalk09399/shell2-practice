0375966282fbd0728942f94a873f9a41214fb8a0
nishal <nishalk9399@gmail.com>
2023-08-18 15:15:59 +0530

    variables

--- 06-Arrays.sh
+++ 06-Arrays.sh
@@ -2,8 +2,8 @@
 
 #Arrays declaration 
 
-PERSONS=("ramesh" "suresh" "mahesh")
+PERSONS=("ramesh" "suresh" "mahesh" "nishal")
 
 echo "All persons: ${PERSONS[@]}"
 # echo "third person: ${PERSONS[2]}"
-# echo "sixth person: ${PERSONS[4]}"
+echo "third person: ${PERSONS[3]}"
