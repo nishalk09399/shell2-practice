d9cb06eec704240905e83fd48a1fe358e08f7add
nishal <nishalk9399@gmail.com>
2023-08-18 15:29:33 +0530

    variables

---
+++ 07-conditions.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#check the number it is greater than 10 or not. write a shell script for this 
+
+NUMBER=$1
+if($NUMBER -gt 10)
+then
+echo "$NUMBER is greater then 10"
+else
+echo "$NUMBER is not greater then 10"
+fi
