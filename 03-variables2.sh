1851c41d0754e87f86a47758a230ee78a2543c44
nishal <nishalk9399@gmail.com>
2023-08-18 14:20:04 +0530

    variables

--- 03-variables2.sh
+++ 03-variables2.sh
@@ -3,6 +3,8 @@
 person1=$1
 person2=$2
 
+# command line interface we can give the values in the command line itself
+
 echo "$person1: Hi $person2. GM"
 echo "$person2: GM $person1".
 echo "$person1: How are youy $person2".
