0ab0a82aa0759df5e85c17debfa365b74899486c
nishal <nishalk9399@gmail.com>
2023-08-18 13:18:28 +0530

    variables

--- 03-variables2.sh
+++ 03-variables2.sh
@@ -3,7 +3,7 @@
 person1=$1
 person2=$2
 
-echo "$person1: Hi $person2. GM
+echo "$person1: Hi $person2. GM"
 echo "$person2: GM $person1".
 echo "$person1: How are youy $person2".
 echo "$person2: Fine $person1"
