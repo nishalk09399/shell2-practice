b1ad32f727f82e6b762d543dcdd50b86a62b6b76
nishal <nishalk9399@gmail.com>
2023-08-18 15:31:41 +0530

    addition

--- 07-conditions.sh
+++ 07-conditions.sh
@@ -3,7 +3,8 @@
 #check the number it is greater than 10 or not. write a shell script for this 
 
 NUMBER=$1
-if($NUMBER -gt 10)
+
+if[ $NUMBER -gt 10 ]
 then
 echo "$NUMBER is greater then 10"
 else
