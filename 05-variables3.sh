b7be08075f6445405fd6b443ef6f916f515a6b8f
nishal <nishalk9399@gmail.com>
2023-08-18 14:38:55 +0530

    variables

---
+++ 05-variables3.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Ask user name and password script
+
+echo "Please enter your username"
+
+read USERNAME #this is the variable that value stored which is given by user. 
+
+echo "username entered is: $USERNAME"
