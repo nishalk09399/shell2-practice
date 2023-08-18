b9bdf1e1efb6c941aa05eb694563fe0f25238fd1
nishal <nishalk9399@gmail.com>
2023-08-18 21:11:47 +0530

    root

---
+++ 10-specialvariables.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+VAR1=$1
+VAR2=$2
+
+echo "script name: $0"
+
+echo "variable 1: $VAR1"
+
+echo "variable 2: $VAR2"
+
+echo "all variables : $@"
+
+echo "No. of variables present: $#"
