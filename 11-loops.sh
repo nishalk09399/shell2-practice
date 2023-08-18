0af726ac631e5d58502083b7f08037488f9e7b18
nishal <nishalk9399@gmail.com>
2023-08-18 21:34:52 +0530

    root

--- 11-loops.sh
+++ 11-loops.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-echo 1
-echo 2
-echo 3
-echo 4 
-echo 5
+# echo 1
+# echo 2
+# echo 3
+# echo 4 
+# echo 5
+
+#create a loop to print the numbers from 1 to 100
+
+for i in {1..50}; do
+    echo $i
+done
