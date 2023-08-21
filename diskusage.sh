2673818037ae83b0659b6ac22e7db2cc88cb8c50
nishal <nishalk9399@gmail.com>
2023-08-21 20:03:27 +0530

    diskadd

--- diskusage.sh
+++ diskusage.sh
@@ -6,41 +6,35 @@
 
 LOGFILE_DIRECTORY=/tmp
 DATE=$(date +%F:%H:%M:%S)
-
 SCRIPT_NAME=$0
-LOG_FILE=$LOGFILE_DIRECTORY/$SCRIPT_NAME-$DATE.log
+LOGFILE=$LOGFILE_DIRECTORY/$SCRIPT_NAME-$DATE.log
 
-R="\e[31m" 
-G="\e[32m" 
-Y="\e[33m"
+R="\e[31m"
+G="\e[32m"
 N="\e[0m"
+Y="\e[33m"
 
 DISK_USAGE=$(df -hT | grep -vE 'tmpfs|Filesystem')
-DISK_USAGE_TRESHOLD=1
+DISK_USAGE_THRESHOLD=1
+message=""
 
+#IFS= means internal feild seperator is space.
 while IFS= read line
-do
-    #this command will give you disk usage in number format for comparision
+do  
+    # this command will give you usage in number format for comparision
     usage=$(echo $line | awk '{print $6}' | cut -d % -f1)
-
-    #this command will give you the which partioner is consuming memory
+    # this command will give us partition
     partition=$(echo $line | awk '{print $1}')
-
-    #now you need to check wheather it is more then treshold or not
-    if [ $usage -gt $DISK_USAGE_TRESHOLD ];
+    #now you need to check whether it is more than threshold or not
+    if [ $usage -gt $DISK_USAGE_THRESHOLD ];
     then
-        message+="HIGH DISK USAGE ON $partition: $usage\n"
-
+        message+="HIGH DISK USAGE on $partition: $usage\n"
     fi
-
 done <<< $DISK_USAGE
 
 echo -e "message: $message"
 
-#echo  "$message" | mail -s "High Disk usage" nishalk9399@gmail.com
+#echo "$message" | mail -s "High Disk usage" info@joindevops.com
 
-#how to call other shell script from your current script - that is with .sh
-
-sh mail.sh nishalk9399@gmail.com "High disk usage" "$message" "DEVOPS TEAM" "High disk usage"
-
-
+#how to call other shell script from your current script
+sh mail.sh nishalk9399@gmail.com "High Disk Usage" "$message" "DEVOPS TEAM" "High Disk usage"
