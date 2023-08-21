1e072fe3c9b59f03e3f683c3779d9e772b4d818b
nishal <nishalk9399@gmail.com>
2023-08-21 11:57:35 +0530

    loops

--- 12-loops2.sh
+++ 12-loops2.sh
@@ -10,7 +10,9 @@ LOG_FILE=$LOG_DIR/$0-$DATE.log
 
 USERID=$(id -u)
 R="\e[31m" 
+G="\e[32m" 
 Y="\e[33m"
+
 
 
 if [ $USERID -ne 0 ]
@@ -19,6 +21,16 @@ then
    exit 1
 fi
 
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then 
+        echo -e "Installing $2 .... $R Failure"
+        exit1
+    else
+        echo -e "Intsllaing $2 .....$G Success"
+    fi
+}
+
 for i in $@
 do 
     yum list installed $i &>>$LOG_FILE
@@ -26,6 +38,7 @@ do
     then 
         echo "$i is not avilable, pls install it"
         yum install $i -y &>>$LOG_FILE
+        VALIDATE $? "$i"
     else
         echo -e "$Y package is already installed"
     fi
